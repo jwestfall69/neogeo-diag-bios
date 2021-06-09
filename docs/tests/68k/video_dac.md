b5655029eae2d393d27f510e85f3e76101e2744b
jwestfall <jwestfall@surrealistic.net>
2021-06-08 20:22:31 -0700

    Add Video DAC Test

---
+++ docs/tests/68k/video_dac.md
@@ -0,0 +1,109 @@
+# Video DAC Test
+---
+The video DAC test can help track down color issues by giving both an overview
+of each of the individual digital input signals and being able to isolate those
+input signals.
+
+## Overview
+Some simple background info related to this test
+
+### Colors
+Each color value on the NEO GEO is 16 bits, with 5 bits for each red, green and
+blue.  The remaining bit is referred to as the dark(er) bit and according to
+the NEO GEO development docs is suppose to do an RGB - 1 effect (almost none).
+
+For more in depth details refer to the [Colors](https://wiki.neogeodev.org/index.php?title=Colors)
+page on the [Neo-Geo Dev Wiki](https://wiki.neogeodev.org/index.php?title=Main_Page).
+
+
+### Shadow/No Shadow Registers
+A screen wide shadow (darkening) effect can be applied via the REG_SHADOW or
+removed via the REG_NOSHADOW registers.  
+
+### Video DAC
+Each of the analog RGB signals are generated from 7 digital signals.  The weight
+that each of these digital signals play in generating the RGB signal is
+determined by the resistor connected to it.  The lower the resistor's ohm value
+the higher the weight.
+
+|  Digital Signal |  Resistor |
+|:---------------:|:---------:|
+| Color Bit 0     | 3.9k ohm  |
+| Color Bit 1     | 2.2k ohm  |
+| Color Bit 2     |   1k ohm  |
+| Color Bit 3     |  470 ohm  |
+| Color Bit 4     |  220 ohm  |
+| Dark(er) Bit    | 8.2k ohm  |
+| Shadow Register |  150 ohm  |
+
+For more in depth details refer to the [Video DAC](https://wiki.neogeodev.org/index.php?title=Video_DAC) page on the [Neo-Geo Dev Wiki](https://wiki.neogeodev.org/index.php?title=Main_Page).
+
+## Main Screen
+This screen has a grid of color blocks that provides a quick glance to see if
+there is any issue with a specific color bit.
+
+![video dac main](images/video_dac_main.png)
+
+The B0 B1 .. B4 heading at the top of the grid is indicating which individual
+color bit is enabled for that column, while the 'ALL' column has all bits for
+that color enabled.  The 4th row is the combination of the 3 previous rows. ie:
+the B2 column of the 4th row has color bit 2 enabled for red, green, and blue.
+
+|   Button   | Description |
+|:----------:| :-----------|
+|     A      | Enter full screen mode |
+|     B      | Toggles dark(er) bit.  This will cause no visual change to the screen, but you should be able to detect it if you use a logic probe the DAC's 3x 8.2K ohm resistors. |
+|     C      | Toggle shadow register effect.  When the screen is in its normal state you should see pulsing on the DAC's 3x 150 ohm resistors.  When it goes darker the DAC's 3x 150 ohm resistors should be 100% low. |
+|     D      | Return to main menu |
+
+## Full Screen
+Full screen mode fills the screen with a single block from the grid.  This makes
+it possible to isolate an individual color bit since it will be on the only
+color on the screen.  When entering full screen mode you will start out at
+the color from the top left of the grid (red B0).  Using left/right/up/down
+you can switch the color that fills the screen, which will change based on the
+color block grid from the main Video DAC screen.
+
+You should expect to see pulsing on the input of the associated resistor for
+the selected color bit and color.  ie: if you are on color bit 2 on red you
+should only see pulsing on the input of red's 1k ohm resistor.
+
+|   Button   | Description |
+|:----------:| :-----------|
+| Left/Right | Cycle through color bits / all columns as seen on the grid on the main screen |
+|  Up/Down   | Cycle through red, green, blue and all rows as seen on the grid on the main screen |
+|     B      | Toggles dark(er) bit.  This will cause no visual change to the screen. <br><br> **IMPORTANT**: If you are using a logic probe instead of and oscilloscope, you will want to validate this bit using the color bit 4 (B4) color set.  The lower color bits will likely always show the input to the 8.2k resistor(s) as being low. <br><br>If viewing red, green or blue you should expect to only see an effect on that color's 8.2K resistor.  While viewing the combined colors on all 3x 8.2k resistors. |
+|     C      | Toggle the shadow register effect.  You should see the screen get darker then lighter as you toggle it repeatedly.<br><br> **IMPORTANT**: If you are using a logic probe instead of and oscilloscope, you will want to validate this bit using the color bit 4 (B4) color set.  The lower color bits will likely always show the input to the 150 resistor(s) as being low.<br><br>If viewing red, green or blue you should expect to only see an effect on that color's 150 resistor.  While viewing the combined colors on all 3x 150 resistors.|
+|     D      | Return to Video DAC main screen |
+
+
+## Examples
+
+### MV6 with a light yellowish tint to it
+
+MV6 has a light yellowish tint to it that you could mainly see on white. On the
+Video DAC main screen nothing really looked to out of the ordinary with
+red/green/blue rows, but the text and 4th row had the yellowish tint.
+
+When toggling the shadow register on the main screen it got darker as expected,
+but also the yellowish tint went away causing the text/4th row to be a gray
+color.  Upon toggling it a number of times it became apparent the red and blue
+rows were not switching between being darker/normal.  This was confirmed by
+using a logic probe on red/blue's 150 ohm resistor and seeing it stuck low
+when toggling the shadow register.  With them being stuck low it would cause
+them to always be in the darkened state.
+
+From there the issue was back tracked to the 74LS05 (hex inverter) that feeds
+the 150 ohms resistors (and 8.2k resistors).  The input on the 74LS05 was
+flipping high/low when toggling the shadow register, but the output on it for
+red/blue were always low.  Swapping the 74LS05 fixed the issue.
+
+
+### MV4 colors are a bit off
+The colors are a bit off on the MV4.  Going to the Video DAC main screen the
+red B4 color block is missing (black).  Measuring red's 220 ohm resister with
+a multi-meter show its OL.  Replacing the 200 ohm resistor fixes the issue.
+
+The same type of missing color block can be caused by the input from the
+74LS273 being stuck low.  This would indicated the 74LS273 is bad or the
+input coming into it is.
--- sp1/sp1.asm
+++ sp1/sp1.asm
@@ -82,9 +82,13 @@ fix_clear_line_ssa3:
 
 ; clears the fix layer
 fix_clear_ssa3:
+	move.w	#$20, d0
+
+; fills the entire fix layer with a tile
+; d0 = tile
+fix_fill_ssa3:
 	move.w	#FIXMAP, (-2,a6)
 	move.w	#1, (2,a6)
-	move.w	#$20, d0
 	move.w	#$4ff, d1
 .loop_next_tile:
 	move.w	d0, (a6)
@@ -1828,6 +1832,7 @@ MAIN_MENU_ITEMS_START:
 	MAIN_MENU_ITEM STR_MM_CALENDAR_IO, manual_calendar_test, 1
 	MAIN_MENU_ITEM STR_MM_COLOR_BARS, manual_color_bars_test, 0
 	MAIN_MENU_ITEM STR_MM_SMPTE_COLOR_BARS, manual_smpte_color_bars_test, 0
+	MAIN_MENU_ITEM STR_MM_VIDEO_DAC_TEST, manual_video_dac_test, 0
 	MAIN_MENU_ITEM STR_MM_CONTROLER_TEST, manual_controller_test, 0
 	MAIN_MENU_ITEM STR_MM_WBRAM_TEST_LOOP, manual_wbram_test_loop, 0
 	MAIN_MENU_ITEM STR_MM_PAL_RAM_TEST_LOOP, manual_palette_ram_test_loop, 0
@@ -3527,6 +3532,317 @@ smpte_color_bar_draw_section:
 	dbra	d2, .loop_next_row
 	rts
 
+; main vdac screen
+; A button = enter full screen mode
+; B button = toggle darker bit
+; C button = toggle shadow register
+; D button = return to main menu
+manual_video_dac_test:
+
+	moveq	#0, d6			; will use d6 to track shadow toggle
+	move.b	d0, REG_NOSHADOW
+
+	bsr	video_dac_setup_palettes
+	bsr	video_dac_draw_screen
+
+.loop_run_test
+	WATCHDOG
+	bsr	p1p2_input_update
+	bsr	wait_frame
+
+.right_not_pressed:
+	btst	#A_BUTTON, p1_input_edge
+	beq	.a_not_pressed
+	bsr	video_dac_draw_fullscreen
+
+	bra	manual_video_dac_test		; jump to the stop so we clear shadow/darker bit
+
+.a_not_pressed:
+	btst	#B_BUTTON, p1_input_edge
+	beq	.b_not_pressed
+	bsr	video_dac_toggle_darker_bit
+
+.b_not_pressed:
+
+	btst	#C_BUTTON, p1_input_edge
+	beq	.c_not_pressed
+	bsr	video_dac_toggle_reg_shadow
+
+.c_not_pressed:
+	btst	#D_BUTTON, p1_input_edge	; D pressed?
+	beq	.loop_run_test
+
+	; we dont need to worry about cleaning up palettes, but
+	; we should make sure show is off.
+	move.b	d0, REG_NOSHADOW
+	rts
+
+VDAC_FS_TILE_OFFSETS:
+	dc.w	$0000, $0020, $6000, $6020
+
+VDAC_FS_TILE_BASE_PAL_MIN	equ $4000
+VDAC_FS_TILE_BASE_PAL_MAX 	equ $9000
+
+; fill the entire screen with the single tile
+; Left/Right = cycle through color bits / all
+; UP/Down = cycle through red/green/blue/combined
+; B button = toggle darker bit
+; C button = toggle shadow register
+; D button = return to main vdac screen
+video_dac_draw_fullscreen:
+
+	move.w	#VDAC_FS_TILE_BASE_PAL_MIN, d0
+	SSA3	fix_fill				; fills the screen red/color bit 0
+
+	move.w	#VDAC_FS_TILE_BASE_PAL_MIN, d3		; start tile base pal
+	lea	VDAC_FS_TILE_OFFSETS, a0
+	moveq	#0, d4					; tile offset in array
+
+.loop_input:
+	WATCHDOG
+	bsr	p1p2_input_update
+	bsr	wait_frame
+
+	btst	#UP, p1_input_edge
+	beq	.up_not_pressed
+
+	subq.b	#2, d4
+	bpl	.redraw_fullscreen
+	moveq	#6, d4
+	bra	.redraw_fullscreen
+
+.up_not_pressed:
+	btst	#DOWN, p1_input_edge
+	beq	.down_not_pressed
+
+	addq.b	#2, d4
+	cmp.b	#8, d4
+	bne	.redraw_fullscreen
+	moveq	#0, d4
+	bra	.redraw_fullscreen
+
+.down_not_pressed:
+	btst	#RIGHT, p1_input_edge
+	beq	.right_not_pressed
+
+	add.w	#$1000, d3
+	cmp.w	#VDAC_FS_TILE_BASE_PAL_MAX + $1000, d3
+	bmi	.redraw_fullscreen
+	move.w	#VDAC_FS_TILE_BASE_PAL_MIN, d3
+	bra	.redraw_fullscreen
+
+.right_not_pressed:
+	btst	#LEFT, p1_input_edge
+	beq	.left_not_pressed
+
+	sub.w	#$1000, d3
+	cmp.w	#VDAC_FS_TILE_BASE_PAL_MIN, d3
+	bpl	.redraw_fullscreen
+	move.w	#VDAC_FS_TILE_BASE_PAL_MAX, d3
+
+.redraw_fullscreen:
+	moveq	#0, d0
+	move.w	(a0,d4), d0
+	add.w	d3, d0			; tile base pal + tile offset = what to fill with
+	SSA3	fix_fill
+
+.left_not_pressed:
+	btst	#B_BUTTON, p1_input_edge
+	beq	.b_not_pressed
+	movem.l d0-d1/a0, -(a7)
+	bsr	video_dac_toggle_darker_bit
+	movem.l (a7)+, d0-d1/a0
+
+.b_not_pressed:
+	btst	#C_BUTTON, p1_input_edge
+	beq	.c_not_pressed
+	bsr	video_dac_toggle_reg_shadow
+
+.c_not_pressed
+	btst	#D_BUTTON, p1_input_edge
+	beq	.loop_input
+
+	rts
+
+; enabling/disabling this bit in the palette doesn't make
+; any visual difference on screen, but you can hear it on the
+; 8.2k resistors on the dac
+video_dac_toggle_darker_bit:
+
+	lea	PALETTE_RAM_START+(PALETTE_SIZE*5)+$2, a0
+	moveq	#9, d0
+
+.loop_next_pallete:
+	move.l	(a0), d1
+	eor.l	#$80008000, d1
+	move.l	d1, (a0)
+	adda.l	#PALETTE_SIZE, a0
+	dbra	d0, .loop_next_pallete
+	rts
+
+; when shadow is enabled it should cause the 150ohm resister on
+; the active color(s) to be low 100% of the time.  When shadow is
+; disabled the resister will have a bit of a pulse to it on the active
+; color(s)
+video_dac_toggle_reg_shadow:
+
+	eor.b	#1, d6
+	beq	.disable_reg_shadow
+
+	move.b	d0, REG_SHADOW
+	rts
+
+.disable_reg_shadow
+	move.b	d0, REG_NOSHADOW
+	rts
+
+; palettes  5 to  9 are used by red  and green
+; palettes 10 to 14 are used by blue and combined
+video_dac_setup_palettes:
+
+	lea	PALETTE_RAM_START+(PALETTE_SIZE*4)+$2, a0	; goto palette5 color1
+	move.l	#$40002000, d0					; lsb redgreen
+	move.l	#$01000010, d1					; red/green
+	move.l	#$4f0020f0, d2					; full red/green
+	bsr	video_dac_setup_palette_group
+
+	lea     PALETTE_RAM_START+(PALETTE_SIZE*10)+$2, a0	; goto palette10 color1
+	move.l	#$10007000, d0					; lsb blue/combined
+	move.l	#$00010111, d1					; blue/combined
+	move.l	#$100f7fff, d2					; full blue/white
+	bsr	video_dac_setup_palette_group
+
+	rts
+
+; setup an individual palette group
+; a0 = palette start address
+; d0 = lsb color
+; d1 = start normal color
+; d2 = all bits
+video_dac_setup_palette_group:
+	; lsb bits
+	move.l	d0, (a0)
+	adda.l	#PALETTE_SIZE, a0
+
+	moveq	#3, d0						; 4 rol palettes
+
+
+.loop_next_palette
+	move.l	d1, (a0)
+	rol.l	#1, d1
+	adda.l	#PALETTE_SIZE, a0				; next palette/color1
+	dbra	d0, .loop_next_palette
+
+	move.l	d2, (a0)					; all bits
+	rts
+
+
+; draw the main screen
+video_dac_draw_screen:
+	SSA3	fix_clear
+
+	lea	STR_MM_VIDEO_DAC_TEST, a0
+	moveq	#13, d0
+	moveq	#3, d1
+	RSUB	print_xy_string
+
+	lea	XY_STR_VDAC_A_FULL_SCREEN, a0
+	RSUB	print_xy_string_struct_clear
+
+	lea	XY_STR_VDAC_B_TOGGLE_DB, a0
+	RSUB	print_xy_string_struct_clear
+
+	lea	XY_STR_VDAC_C_TOGGLE_SHADOW, a0
+	RSUB	print_xy_string_struct_clear
+
+	lea	XY_STR_VDAC_D_MAIN_MENU, a0
+	RSUB	print_xy_string_struct_clear
+
+	lea	XY_STR_VDAC_ALL, a0
+	RSUB	print_xy_string_struct_clear
+
+	; print the B0 B1 ... B4 header (backwards)
+	moveq	#4, d5		; bits to print
+	moveq	#26, d4		; start X offset
+.loop_next_print_header_bit:
+
+	move.b	d4, d0
+	moveq	#6, d1
+	SSA3	fix_seek_xy
+
+	moveq	#0, d1
+	move.l	d5, d2
+	RSUB	print_digits
+
+	move.w	#'B', (a6)
+	sub.b	#4, d4
+	dbra	d5, .loop_next_print_header_bit
+
+	; draw the red/green rows
+	moveq	#8, d0
+	moveq	#7, d1
+	SSA3	fix_seek_xy
+
+	move.w	#$4000, d1
+	bsr	video_dac_draw_color_pair
+
+	; draw the blue/combined rows
+	moveq	#8, d0
+	moveq	#15, d1
+	SSA3	fix_seek_xy
+
+	move.w	#$a000, d1
+	bsr	video_dac_draw_color_pair
+
+	rts
+
+; d0 = column start
+; d1 = start palette
+video_dac_draw_color_pair:
+	move.w	#1, (2,a6)
+
+	move.w	d1, d4			; 0x00 tile
+	move.w	d1, d5			; 0x20 tile
+	add.w	#$20, d5
+
+	moveq	#5, d2			; total number of color bits
+
+.loop_next_color_bit:
+
+	moveq	#3, d3			; width of each color
+
+.loop_color_bit_width:
+
+	; tile 0x00 (color1)
+	move.w	d4, (a6)
+	nop
+	move.w	d4, (a6)
+	nop
+	move.w	d4, (a6)
+	nop
+	move.w	#$20, (a6)
+	nop
+
+	; tile 0x20 (color2)
+	move.w	d5, (a6)
+	nop
+	move.w	d5, (a6)
+	nop
+	move.w	d5, (a6)
+	nop
+	move.w	#$20, (a6)
+
+	add.w	#$20, d0
+	move.w	d0, (-2,a6)			; move over a column
+	dbra	d3, .loop_color_bit_width
+
+	add.w	#$1000, d4			; next palette
+	add.w	#$1000, d5
+
+	dbra	d2, .loop_next_color_bit
+	rts
+
+
 manual_controller_test:
 	moveq	#$5, d0
 	SSA3	fix_clear_line
@@ -4883,6 +5199,7 @@ STR_MC_ADDRESS:			STRING "MEMCARD ADDRESS"
 STR_MM_CALENDAR_IO:		STRING "CALENDAR I/O (MVS ONLY)"
 STR_MM_COLOR_BARS:		STRING "COLOR BARS"
 STR_MM_SMPTE_COLOR_BARS:	STRING "SMPTE COLOR BARS"
+STR_MM_VIDEO_DAC_TEST:		STRING "VIDEO DAC TEST"
 STR_MM_CONTROLER_TEST:		STRING "CONTROLLER TEST"
 STR_MM_WBRAM_TEST_LOOP:		STRING "WRAM/BRAM TEST LOOP"
 STR_MM_PAL_RAM_TEST_LOOP:	STRING "PALETTE RAM TEST LOOP"
@@ -4898,6 +5215,13 @@ XY_STR_CAL_C_4096HZ_PULSE:	XY_STRING  4, 12, "C: 4096Hz pulse"
 XY_STR_CAL_D_MAIN_MENU:		XY_STRING  4, 14, "D: Return to menu"
 XY_STR_CAL_4990_TP:		XY_STRING  4, 21, "4990 TP:"
 XY_STR_CAL_WAITING_PULSE:	XY_STRING  4, 27, "WAITING FOR CALENDAR PULSE..."
+
+; strings for video dac test
+XY_STR_VDAC_A_FULL_SCREEN:	XY_STRING  4, 24, "A: Toggle Full Screen"
+XY_STR_VDAC_B_TOGGLE_DB:	XY_STRING  4, 25, "B: Toggle Darker Bit"
+XY_STR_VDAC_C_TOGGLE_SHADOW:	XY_STRING  4, 26, "C: Toggle Shadow Register"
+XY_STR_VDAC_D_MAIN_MENU:	XY_STRING  4, 27, "D: Return to menu"
+XY_STR_VDAC_ALL:		XY_STRING 29,  6, "ALL"
 
 ; strings for controller test screen
 XY_STR_CT_D_MAIN_MENU:		XY_STRING  4, 27, "D: Return to menu"
