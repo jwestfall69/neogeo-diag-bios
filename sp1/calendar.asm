89cde03b711aa4a4f553f42b8cec4814992a3cb2
jwestfall <jwestfall@surrealistic.net>
2021-07-15 12:32:38 -0700

    split off calendar tests

---
+++ sp1/calendar.asm
@@ -0,0 +1,188 @@
+	include "neogeo.inc"
+	include "macros.inc"
+	include "sp1.inc"
+
+	global manual_calendar_tests
+	global STR_CALENDAR_IO
+
+	section	text
+
+manual_calendar_tests:
+		lea	XY_STR_A_1HZ_PULSE, a0
+		RSUB	print_xy_string_struct_clear
+		lea	XY_STR_B_64HZ_PULSE, a0
+		RSUB	print_xy_string_struct_clear
+		lea	XY_STR_C_4096HZ_PULSE, a0
+		RSUB	print_xy_string_struct_clear
+		lea	XY_STR_D_MAIN_MENU, a0
+		RSUB	print_xy_string_struct_clear
+
+		moveq	#$4, d0
+		moveq	#$11, d1
+		lea	STR_ACTUAL, a0
+		RSUB	print_xy_string_clear
+
+		moveq	#$4, d0
+		moveq	#$13, d1
+		lea	STR_EXPECTED, a0
+		RSUB	print_xy_string_clear
+
+		lea	XY_STR_4990_TP, a0
+		RSUB	print_xy_string_struct_clear
+
+		bsr	rtc_set_1_hz
+
+		bsr	p1_input_update
+
+	.loop_run_test:
+		WATCHDOG
+		bsr	p1_input_update
+
+		bsr	rtc_print_data
+		move.b	p1_input_edge, d0
+		add.b	d0, d0
+		bcs	.test_exit			; d pressed, exit test
+
+		add.b	d0, d0
+		bcc	.c_not_pressed			; check for c pressed
+		bsr	rtc_set_4096_hz
+		bra	.loop_run_test
+	.c_not_pressed:
+
+		add.b	d0, d0
+		bcc	.b_not_pressed
+		bsr	rtc_set_64_hz
+		bra	.loop_run_test
+	.b_not_pressed:
+
+		add.b	d0, d0
+		bcc	.loop_run_test
+		bsr	rtc_set_1_hz
+		bra	.loop_run_test
+
+	.test_exit:
+		move	#$2700, sr			; disable interrupts
+		move.w	#$0, ($4,a6)			; disable timer
+		move.w	#$2, ($a,a6)			; ack timer interrupt
+		rts
+
+rtc_set_1_hz:
+		moveq	#$8, d0
+		move.l	#$5b8d80, d1
+		bra	rtc_update_hz
+
+rtc_set_64_hz:
+		moveq	#$4, d0
+		move.l	#$16e36, d1
+		bra	rtc_update_hz
+
+rtc_set_4096_hz:
+		moveq	#$7, d0
+		move.l	#$5b8, d1
+
+rtc_update_hz:
+		move.w	#$20, ($4,a6)		; Reload counter as soon as REG_TIMERLOW is written to
+		clr.w	timer_count
+		bsr	rtc_send_command
+
+		move.l	d1, -(a7)
+		lea	XY_STR_WAITING_PULSE, a0
+		RSUB	print_xy_string_struct_clear
+
+		bsr	rtc_wait_pulse
+
+		moveq	#$1b, d0
+		SSA3	fix_clear_line		; removes waiting for calendar pulse... line
+
+		move.l	(a7)+, ($6,a6)		; timer high
+		move.w	#$90, ($4,a6)		; lspcmode
+		move	#$2100, sr		; enable interrupts
+		moveq	#$0, d2			; zero out pulse counter
+		rts
+
+; d2 = number of pulses
+rtc_print_data:
+		moveq	#$e, d0
+		moveq	#$11, d1
+		move.w	d2, -(a7)
+		RSUB	print_hex_word
+
+		moveq	#$e, d0
+		moveq	#$13, d1
+		move.w	timer_count, d2
+		RSUB	print_hex_word
+
+		moveq	#$e, d0
+		moveq	#$15, d1
+		SSA3	fix_seek_xy
+
+		moveq	#$18, d0
+		move.b	REG_STATUS_A, d1
+		add.b	d1, d1
+		add.b	d1, d1
+		addx.b	d0, d0
+		move.w	d0, (a6)
+
+		move.w	(a7)+, d2
+		bsr	rtc_check_pulse
+		beq	.no_rtc_pulse
+		addq.w	#1, d2
+
+	.no_rtc_pulse:
+		rts
+
+; d0 = data
+; sends the 4 bit command to the rtc, which runs in
+; serial mode (shift register)
+rtc_send_command:
+		move.l	d1, -(a7)
+		lea	REG_RTCCTRL, a0
+		moveq	#$3, d2
+	.loop_next:
+		moveq	#$1, d1
+		and.b	d0, d1
+		move.b	d1, (a0)  	; write bit 0 from d0
+		addq.b	#2, d1
+		nop
+		move.b	d1, (a0)  	; rtc clock high
+		subq.b	#2, d1
+		lsr.b	#1, d0		; shift right d0 to prep for next bit to send (next loop)
+		move.b	d1, (a0)  	; rtc clock low to tigger shift
+		dbra	d2, .loop_next
+		move.b	#$4, (a0)	; rtc stb high
+		nop
+		clr.b	(a0)		; rtc stb low (run command)
+		move.l	(a7)+, d1
+		rts
+
+rtc_wait_pulse:
+		WATCHDOG
+		btst	#$6, REG_STATUS_A
+		bne	rtc_wait_pulse
+
+	.loop_rtc_pulse_low:
+		WATCHDOG
+		btst	#$6, REG_STATUS_A
+		beq	.loop_rtc_pulse_low
+		move.b	#$40, rtc_pulse_state
+		rts
+
+; if there is a new pulse, Z will be set
+rtc_check_pulse:
+		moveq	#$40, d0
+		and.b	REG_STATUS_A, d0
+		move.b	rtc_pulse_state, d1
+		move.b	d0, rtc_pulse_state
+		eor.b	d0, d1
+		and.b	d0, d1
+		rts
+
+STR_CALENDAR_IO:		STRING "CALENDAR I/O (MVS ONLY)"
+
+; strings for calender io screen
+XY_STR_A_1HZ_PULSE:		XY_STRING  4,  8, "A: 1Hz pulse"
+XY_STR_B_64HZ_PULSE:		XY_STRING  4, 10, "B: 64Hz pulse"
+XY_STR_C_4096HZ_PULSE:		XY_STRING  4, 12, "C: 4096Hz pulse"
+XY_STR_D_MAIN_MENU:		XY_STRING  4, 14, "D: Return to menu"
+XY_STR_4990_TP:			XY_STRING  4, 21, "4990 TP:"
+XY_STR_WAITING_PULSE:		XY_STRING  4, 27, "WAITING FOR CALENDAR PULSE..."
--- sp1/sp1.asm
+++ sp1/sp1.asm
@@ -5,12 +5,15 @@
 	include "../common/comm.inc"
 
 	global _start
+	global p1_input_update
 	global p1p2_input_update
+	global send_p1p2_controller
 	global timer_interrupt
 	global vblank_interrupt
 	global wait_frame
 	global wait_scanline
-	global send_p1p2_controller
+	global STR_ACTUAL
+	global STR_EXPECTED
 	global XY_STR_D_MAIN_MENU
 
 	section	text
@@ -1442,7 +1445,7 @@ main_menu_loop:
 ;  word flags,  // 0 = valid for both, 1 = aes disabled, 2 = mvs disable
 ; }
 MAIN_MENU_ITEMS_START:
-	MAIN_MENU_ITEM STR_MM_CALENDAR_IO, manual_calendar_test, 1
+	MAIN_MENU_ITEM STR_CALENDAR_IO, manual_calendar_tests, 1
 	MAIN_MENU_ITEM STR_COLOR_BARS_BASIC, manual_color_bars_basic_test, 0
 	MAIN_MENU_ITEM STR_COLOR_BARS_SMPTE, manual_color_bars_smpte_test, 0
 	MAIN_MENU_ITEM STR_VIDEO_DAC_TESTS, manual_video_dac_tests, 0
@@ -1554,54 +1557,6 @@ calc_crc32_dsub:
 	dbra	d4, .loop_outter
 	not.l	d0
 	DSUB_RETURN
-
-; d0 = data
-; sends the 4 bit command to the rtc, which runs in
-; serial mode (shift register)
-rtc_send_command:
-	move.l	d1, -(a7)
-	lea	REG_RTCCTRL, a0
-	moveq	#$3, d2
-.loop_next:
-	moveq	#$1, d1
-	and.b	d0, d1
-	move.b	d1, (a0)  	; write bit 0 from d0
-	addq.b	#2, d1
-	nop
-	move.b	d1, (a0)  	; rtc clock high
-	subq.b	#2, d1
-	lsr.b	#1, d0		; shift right d0 to prep for next bit to send (next loop)
-	move.b	d1, (a0)  	; rtc clock low to tigger shift
-	dbra	d2, .loop_next
-	move.b	#$4, (a0)	; rtc stb high
-	nop
-	clr.b	(a0)		; rtc stb low (run command)
-	move.l	(a7)+, d1
-	rts
-
-rtc_wait_pulse:
-	WATCHDOG
-	btst	#$6, REG_STATUS_A
-	bne	rtc_wait_pulse
-
-.loop_rtc_pulse_low:
-	WATCHDOG
-	btst	#$6, REG_STATUS_A
-	beq	.loop_rtc_pulse_low
-	move.b	#$40, rtc_pulse_state
-	rts
-
-
-; if there is a new pulse, Z will be set
-rtc_check_pulse:
-	moveq	#$40, d0
-	and.b	REG_STATUS_A, d0
-	move.b	rtc_pulse_state, d1
-	move.b	d0, rtc_pulse_state
-	eor.b	d0, d1
-	and.b	d0, d1
-	rts
-
 
 auto_ram_oe_tests_dsub:
 	lea	WORK_RAM_START.l, a0		; wram upper
@@ -2735,132 +2690,6 @@ check_mmio_oe_word:
 	cmp.w	*-4(PC), d1
 .test_passed:
 	dbeq	d2, .loop_test_again
-	rts
-
-manual_calendar_test:
-	lea	XY_STR_CAL_A_1HZ_PULSE, a0
-	RSUB	print_xy_string_struct_clear
-	lea	XY_STR_CAL_B_64HZ_PULSE, a0
-	RSUB	print_xy_string_struct_clear
-	lea	XY_STR_CAL_C_4096HZ_PULSE, a0
-	RSUB	print_xy_string_struct_clear
-	lea	XY_STR_CAL_D_MAIN_MENU, a0
-	RSUB	print_xy_string_struct_clear
-
-	moveq	#$4, d0
-	moveq	#$11, d1
-	lea	STR_ACTUAL, a0
-	RSUB	print_xy_string_clear
-
-	moveq	#$4, d0
-	moveq	#$13, d1
-	lea	STR_EXPECTED, a0
-	RSUB	print_xy_string_clear
-
-	lea	XY_STR_CAL_4990_TP, a0
-	RSUB	print_xy_string_struct_clear
-
-	bsr	rtc_set_1_hz
-
-	bsr	p1_input_update
-
-.loop_run_test:
-	WATCHDOG
-	bsr	p1_input_update
-
-	bsr	rtc_print_data
-	move.b	p1_input_edge, d0
-	add.b	d0, d0
-	bcs	.test_exit			; d pressed, exit test
-
-	add.b	d0, d0
-	bcc	.c_not_pressed			; check for c pressed
-	bsr	rtc_set_4096_hz
-	bra	.loop_run_test
-
-.c_not_pressed:
-	add.b	d0, d0
-	bcc	.b_not_pressed
-	bsr	rtc_set_64_hz
-	bra	.loop_run_test
-
-.b_not_pressed:
-	add.b	d0, d0
-	bcc	.loop_run_test
-	bsr	rtc_set_1_hz
-	bra	.loop_run_test
-
-.test_exit:
-	move	#$2700, sr			; disable interrupts
-	move.w	#$0, ($4,a6)			; disable timer
-	move.w	#$2, ($a,a6)			; ack timer interrupt
-	rts
-
-rtc_set_1_hz:
-	moveq	#$8, d0
-	move.l	#$5b8d80, d1
-	bra	rtc_update_hz
-
-rtc_set_64_hz:
-	moveq	#$4, d0
-	move.l	#$16e36, d1
-	bra	rtc_update_hz
-
-
-rtc_set_4096_hz:
-	moveq	#$7, d0
-	move.l	#$5b8, d1
-
-rtc_update_hz:
-	move.w	#$20, ($4,a6)		; Reload counter as soon as REG_TIMERLOW is written to
-	clr.w	timer_count
-	bsr	rtc_send_command
-
-	move.l	d1, -(a7)
-	lea	XY_STR_CAL_WAITING_PULSE, a0
-	RSUB	print_xy_string_struct_clear
-
-	bsr	rtc_wait_pulse
-
-	moveq	#$1b, d0
-	SSA3	fix_clear_line		; removes waiting for calendar pulse... line
-
-	move.l	(a7)+, ($6,a6)		; timer high
-	move.w	#$90, ($4,a6)		; lspcmode
-	move	#$2100, sr		; enable interrupts
-	moveq	#$0, d2			; zero out pulse counter
-	rts
-
-
-; d2 = number of pulses
-rtc_print_data:
-	moveq	#$e, d0
-	moveq	#$11, d1
-	move.w	d2, -(a7)
-	RSUB	print_hex_word
-
-	moveq	#$e, d0
-	moveq	#$13, d1
-	move.w	timer_count, d2
-	RSUB	print_hex_word
-
-	moveq	#$e, d0
-	moveq	#$15, d1
-	SSA3	fix_seek_xy
-
-	moveq	#$18, d0
-	move.b	REG_STATUS_A, d1
-	add.b	d1, d1
-	add.b	d1, d1
-	addx.b	d0, d0
-	move.w	d0, (a6)
-
-	move.w	(a7)+, d2
-	bsr	rtc_check_pulse
-	beq	.no_rtc_pulse
-	addq.w	#1, d2
-
-.no_rtc_pulse:
 	rts
 
 manual_wbram_test_loop:
@@ -4064,22 +3893,12 @@ STR_MC_DATA:			STRING "MEMCARD DATA"
 STR_MC_ADDRESS:			STRING "MEMCARD ADDRESS"
 
 ; main menu items
-STR_MM_CALENDAR_IO:		STRING "CALENDAR I/O (MVS ONLY)"
 STR_MM_WBRAM_TEST_LOOP:		STRING "WRAM/BRAM TEST LOOP"
 STR_MM_PAL_RAM_TEST_LOOP:	STRING "PALETTE RAM TEST LOOP"
 STR_MM_VRAM_TEST_LOOP_32K:	STRING "VRAM TEST LOOP (32K)"
 STR_MM_VRAM_TEST_LOOP_2K:	STRING "VRAM TEST LOOP (2K)"
 STR_MM_MISC_INPUT_TEST:		STRING "MISC. INPUT TEST"
 STR_MM_MEMCARD_TESTS:		STRING "MEMORY CARD TESTS"
-
-; strings for calender io screen
-XY_STR_CAL_A_1HZ_PULSE:		XY_STRING  4,  8, "A: 1Hz pulse"
-XY_STR_CAL_B_64HZ_PULSE:	XY_STRING  4, 10, "B: 64Hz pulse"
-XY_STR_CAL_C_4096HZ_PULSE:	XY_STRING  4, 12, "C: 4096Hz pulse"
-XY_STR_CAL_D_MAIN_MENU:		XY_STRING  4, 14, "D: Return to menu"
-XY_STR_CAL_4990_TP:		XY_STRING  4, 21, "4990 TP:"
-XY_STR_CAL_WAITING_PULSE:	XY_STRING  4, 27, "WAITING FOR CALENDAR PULSE..."
-
 
 ; strings wram/bram test screens
 XY_STR_WBRAM_PASSES:		XY_STRING  4, 14, "PASSES:"
