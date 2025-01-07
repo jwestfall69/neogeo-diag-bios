6c778050bf31d96b176fd64aed8f7c9f90330b1b
Jim Westfall <commits@jwestfall.net>
2025-01-06 18:40:54 -0800

    m1: little cleanup with error addresses

--- m1/src/error_address.asm
+++ m1/src/error_address.asm
@@ -1,3 +1,4 @@
+	include "diag.inc"
 	include "macros.inc"
 
 	global error_address
@@ -7,19 +8,18 @@
 ; params
 ;  a = error code
 ; The z80 uses A0 to A6 for memory refreshing so they will always
-; be pulsing.  This leaves us with 4 useable address lines (A7 to A12)
+; be pulsing.  This leaves us with 6 useable address lines (A7 to A12)
 ; to communicate the error code to the user.
 error_address:
 		and	$3f		; error codes can only be 6 bits
-
-		ld	bc, $a000	; base of error addresses
 
 		ld	hl, $0		; make the error code be bits 7 to 12 of hl
 		ld	h, a
 		srl	h
 		rr	l
 
-		add	hl, bc		; add on base address
+		ld	bc, ERROR_ADDRESS_BASE
+		add	hl, bc
 
 		; The top bit of the R register is used for PSUB nesting.  We need
 		; to make sure its 0 or it will make address line a7 participate
