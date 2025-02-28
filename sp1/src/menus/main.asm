92d56e09f82a68179fc615f7ecff38d1336d4e5c
Jim Westfall <commits@jwestfall.net>
2025-02-27 19:21:35 -0800

    Get rid of debug builds, just compile in the debug menu/items

--- sp1/src/menus/main.asm
+++ sp1/src/menus/main.asm
@@ -46,11 +46,7 @@ d_menu_list:
 	MENU_ENTRY manual_prog_rom_bus_tests, d_str_prog_rom_bus_tests, 0
 	MENU_ENTRY memory_viewer_menu, d_str_memory_viewer, 0
 	MENU_ENTRY fix_tile_viewer, d_str_fix_tile_viewer, 0
-
-	ifd _DEBUG_BUILD_
-		MENU_ENTRY debug_menu, d_str_debug_menu, 0
-	endif
-
+	MENU_ENTRY debug_menu, d_str_debug_menu, 0
 	MENU_LIST_END
 
 d_xys_menu_title:		XY_STRING LEFT_MARGIN, 5, "MAIN MENU"
@@ -61,6 +57,7 @@ d_str_color_bars_basic:		STRING "COLOR BARS BASIC"
 d_str_color_bars_smpte:		STRING "COLOR BARS SMPTE"
 d_str_controller_tests:		STRING "CONTROLLER TESTS"
 d_str_cpu_pal_addr_test:	STRING "CPU/PAL ADDR TEST"
+d_str_debug_menu:		STRING "DEBUG MENU"
 d_str_fix_tile_viewer:		STRING "FIX TILE VIEWER"
 d_str_memcard_tests:		STRING "MEMORY CARD TESTS"
 d_str_memory_viewer:		STRING "MEMORY VIEWER"
@@ -71,7 +68,3 @@ d_str_video_dac_tests:		STRING "VIDEO DAC TESTS"
 d_str_vram_2k_tests:		STRING "VRAM 2K TESTS"
 d_str_vram_32k_tests:		STRING "VRAM 32K TESTS"
 d_str_work_ram_test_loop:	STRING "WORK RAM TEST LOOP"
-
-	ifd _DEBUG_BUILD_
-d_str_debug_menu:		STRING "DEBUG MENU"
-	endif
